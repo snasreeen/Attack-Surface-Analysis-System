3833f9dd3ec22a5a225eda9ab7035d2a9ff44ad9
snasreeen <snasreen7890@gmail.com>
2026-04-25 19:51:58 +0530

    fixed render port issue

--- app.py
+++ app.py
@@ -366,5 +366,4 @@ def check_session():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=5000)
